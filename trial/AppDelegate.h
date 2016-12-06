ce994621c16fc171e8ce98b25fcb592081a5fb47
Abhishek PM <abhishek@abhisheks-macbook-pro.local>
2016-12-06 21:50:03 +0530

    Initial Commit

---
+++ trial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trial
+//
+//  Created by Abhishek on 06/12/16.
+//  Copyright © 2016 Dexinnovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  trial
+//
+//  Created by Abhishek on 06/12/16.
+//  Copyright © 2016 Dexinnovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
